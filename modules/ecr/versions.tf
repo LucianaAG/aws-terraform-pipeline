387118cb986bac45e35ab59b3fb3d929d3476ee2
LucyAG <lucygauto35@gmail.com>
2026-07-18 03:20:00 -0300

    fix: remove duplicate ECR resource from versions.tf

--- modules/ecr/versions.tf
+++ modules/ecr/versions.tf
@@ -1,12 +1,8 @@
-# levantamos el servicio donde se almacenará cada nueva imagen buildeada por el pipeline
-# la instancia EC2 extrae las imagenes de este servicio
-resource "aws_ecr_repository" "app" {
-  name                 = "${lookup(var.tags, "environment", "dev")}-agente-ia"
-  image_tag_mutability = "MUTABLE"
-
-  image_scanning_configuration {
-    scan_on_push = true
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
   }
-
-  tags = var.tags
 }
