d2210aabf96162d40c4fe57a68257c8f6ed4fa5d
LucyAG <lucygauto35@gmail.com>
2026-07-18 03:26:32 -0300

    fix: enable public IP assignment for EC2 instances

--- modules/compute/main.tf
+++ modules/compute/main.tf
@@ -54,6 +54,7 @@ resource "aws_instance" "server" {
   instance_type          = var.instance_type
   vpc_security_group_ids = [var.security_group_id]
   iam_instance_profile   = aws_iam_instance_profile.ssm_profile.name
+  associate_public_ip_address = true
   tags                   = var.tags
 }
 
