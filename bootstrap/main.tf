c6e149226ba18a31a292093d35dfe424940dcc03
LucyAG <lucygauto35@gmail.com>
2026-07-18 03:17:01 -0300

    feat: add GitHub Actions OIDC provider and role with ECR and SSM permissions

--- bootstrap/main.tf
+++ bootstrap/main.tf
@@ -25,6 +25,16 @@ resource "aws_iam_openid_connect_provider" "hcp" {
   thumbprint_list = [data.tls_certificate.hcp.certificates[0].sha1_fingerprint]
 }
 
+# OIDC entre Github Actions y AWS, le pide a AWS que confie en GitHub actions para que pushee las nuevas imagenes buildeadas
+# al repositorio de imagenes ECR
+resource "aws_iam_openid_connect_provider" "github" {
+  url = "https://token.actions.githubusercontent.com"
+
+  client_id_list = ["sts.amazonaws.com"]
+
+  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
+}
+
 # rol que HCP va a asumir
 resource "aws_iam_role" "hcp_role" {
   name = "hcp-terraform-role"
@@ -51,10 +61,48 @@ resource "aws_iam_role" "hcp_role" {
   })
 }
 
+# rol que GitHub Actions va a asumir
+resource "aws_iam_role" "github_actions_role" {
+  name = "github-actions-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Federated = aws_iam_openid_connect_provider.github.arn
+        }
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Condition = {
+          StringEquals = {
+            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+          }
+          StringLike = {
+            "token.actions.githubusercontent.com:sub" = "repo:LucianaAG/agente-ia-corporativo:*"
+          }
+        }
+      }
+    ]
+  })
+}
+
 # definicion de los permisos del rol, acceso completo para poder desplegar infra
 resource "aws_iam_role_policy_attachment" "hcp_policy" {
   role       = aws_iam_role.hcp_role.name
   policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
+
+# definicion de los permisos del rol para github actions, le permite registrar nuevas imagenes
+resource "aws_iam_role_policy_attachment" "github_ecr_policy" {
+  role       = aws_iam_role.github_actions_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryPowerUser"
+}
+
+# definicion de los permisos del rol para github actions, le permite registrar nuevas imagenes
+resource "aws_iam_role_policy_attachment" "github_ssm_policy" {
+  role       = aws_iam_role.github_actions_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMFullAccess"
 }
 
 provider "aws" {
